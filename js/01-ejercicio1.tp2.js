1ba09d595054a002c9a922349d592da3544e585e
Cristian Diaz <diazcristian0210@gmail.com>
2024-01-05 10:47:08 -0300

    Agregue una consigna al ejercicio 1

--- js/01-ejercicio1.tp2.js
+++ js/01-ejercicio1.tp2.js
@@ -1,9 +1,12 @@
-const  edad = parseInt(prompt('Ingrese su edad'))
+/*
+1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+*/
+
+const edad = parseInt(prompt("Ingrese su edad"));
 if (edad >= 18 && edad <= 65) {
-    alert("Puede adquierir un permiso para conducir");
-  } else if (edad < 18) {
-    alert("Eres menor de edad, no puedes solicitar un permiso para conducir");
-  } else {
-    alert("No puede conducir");
-  }
-  
+  alert("Puede adquierir un permiso para conducir");
+} else if (edad < 18) {
+  alert("Eres menor de edad, no puedes solicitar un permiso para conducir");
+} else {
+  alert("No puede conducir");
+}
